9e014fc6aa9446f1969d6d80cc107f36a8f945bb
n00854180t <ross.pawley@gmail.com>
2017-01-04 13:28:15 -0800

    Features update
    Added support for __stdcall functions
    Added support for making multiple detours at once
    Fixed issue with signatures without arguments list

--- SignatureToDetour/signatureToDetourForm.Designer.cs
+++ SignatureToDetour/signatureToDetourForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(signatureToDetourForm));
             this.functionSignatureTextBox = new System.Windows.Forms.TextBox();
             this.makeDetourBtn = new System.Windows.Forms.Button();
             this.outputTextbox = new System.Windows.Forms.TextBox();
@@ -35,26 +36,30 @@
             this.outputNameLabel = new System.Windows.Forms.Label();
             this.functionNameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.addSignatureBtn = new System.Windows.Forms.Button();
+            this.signatureListBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // functionSignatureTextBox
             // 
             this.functionSignatureTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.functionSignatureTextBox.Location = new System.Drawing.Point(12, 114);
+            this.functionSignatureTextBox.Location = new System.Drawing.Point(12, 86);
             this.functionSignatureTextBox.Multiline = true;
             this.functionSignatureTextBox.Name = "functionSignatureTextBox";
             this.functionSignatureTextBox.Size = new System.Drawing.Size(426, 69);
-            this.functionSignatureTextBox.TabIndex = 0;
+            this.functionSignatureTextBox.TabIndex = 1;
+            this.functionSignatureTextBox.Text = "Example: char sub_12345()";
+            this.functionSignatureTextBox.Enter += new System.EventHandler(this.functionSignatureTextBox_Enter);
             // 
             // makeDetourBtn
             // 
             this.makeDetourBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.makeDetourBtn.Location = new System.Drawing.Point(12, 198);
+            this.makeDetourBtn.Location = new System.Drawing.Point(12, 347);
             this.makeDetourBtn.Name = "makeDetourBtn";
             this.makeDetourBtn.Size = new System.Drawing.Size(420, 38);
-            this.makeDetourBtn.TabIndex = 2;
+            this.makeDetourBtn.TabIndex = 4;
             this.makeDetourBtn.Text = "Make Detour";
             this.makeDetourBtn.UseVisualStyleBackColor = true;
             this.makeDetourBtn.Click += new System.EventHandler(this.MakeDetour_Click);
@@ -64,16 +69,16 @@
             this.outputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.outputTextbox.Location = new System.Drawing.Point(12, 275);
+            this.outputTextbox.Location = new System.Drawing.Point(12, 419);
             this.outputTextbox.Multiline = true;
             this.outputTextbox.Name = "outputTextbox";
-            this.outputTextbox.Size = new System.Drawing.Size(425, 286);
-            this.outputTextbox.TabIndex = 3;
+            this.outputTextbox.Size = new System.Drawing.Size(425, 304);
+            this.outputTextbox.TabIndex = 5;
             // 
             // signatureLabel
             // 
             this.signatureLabel.AutoSize = true;
-            this.signatureLabel.Location = new System.Drawing.Point(12, 95);
+            this.signatureLabel.Location = new System.Drawing.Point(12, 67);
             this.signatureLabel.Name = "signatureLabel";
             this.signatureLabel.Size = new System.Drawing.Size(92, 13);
             this.signatureLabel.TabIndex = 5;
@@ -95,22 +100,45 @@
             this.functionNameTextBox.Location = new System.Drawing.Point(12, 41);
             this.functionNameTextBox.Name = "functionNameTextBox";
             this.functionNameTextBox.Size = new System.Drawing.Size(422, 20);
-            this.functionNameTextBox.TabIndex = 7;
+            this.functionNameTextBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 259);
+            this.label1.Location = new System.Drawing.Point(12, 403);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(42, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Output:";
             // 
+            // addSignatureBtn
+            // 
+            this.addSignatureBtn.Location = new System.Drawing.Point(11, 161);
+            this.addSignatureBtn.Name = "addSignatureBtn";
+            this.addSignatureBtn.Size = new System.Drawing.Size(75, 23);
+            this.addSignatureBtn.TabIndex = 2;
+            this.addSignatureBtn.Text = "Add Signature";
+            this.addSignatureBtn.UseVisualStyleBackColor = true;
+            this.addSignatureBtn.Click += new System.EventHandler(this.addSignatureBtn_Click);
+            // 
+            // signatureListBox
+            // 
+            this.signatureListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.signatureListBox.FormattingEnabled = true;
+            this.signatureListBox.Location = new System.Drawing.Point(11, 205);
+            this.signatureListBox.Name = "signatureListBox";
+            this.signatureListBox.Size = new System.Drawing.Size(426, 121);
+            this.signatureListBox.TabIndex = 3;
+            // 
             // signatureToDetourForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 573);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(449, 735);
+            this.Controls.Add(this.signatureListBox);
+            this.Controls.Add(this.addSignatureBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.outputTextbox);
             this.Controls.Add(this.makeDetourBtn);
@@ -118,6 +146,7 @@
             this.Controls.Add(this.outputNameLabel);
             this.Controls.Add(this.signatureLabel);
             this.Controls.Add(this.functionSignatureTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "signatureToDetourForm";
             this.Text = "Signature to Detour Tool";
             this.ResumeLayout(false);
@@ -134,6 +163,8 @@
         private System.Windows.Forms.Label outputNameLabel;
         private System.Windows.Forms.TextBox functionNameTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addSignatureBtn;
+        private System.Windows.Forms.ListBox signatureListBox;
     }
 }
 
--- SignatureToDetour/signatureToDetourForm.cs
+++ SignatureToDetour/signatureToDetourForm.cs
@@ -12,112 +12,188 @@ namespace SignatureToDetour
 {
     public partial class signatureToDetourForm : Form
     {
+        public class Signature
+        {
+            public string name { get; set; }
+            public string signature { get; set; }
+        }
+
+        List<Signature> signatures;
+
         public signatureToDetourForm()
         {
             InitializeComponent();
+            signatures = new List<Signature>();
         }
 
         private void MakeDetour_Click(object sender, EventArgs e)
         {
-            string desired_name = functionNameTextBox.Text;
-            if (desired_name.Length == 0)
-                desired_name = "Unk001";
-
             outputTextbox.Text = "";
 
-            string signature = functionSignatureTextBox.Text; //char __cdecl sub_1193D40(int a1, unsigned int a2, signed int *a3, _BYTE **a4, const char *a5)
-            string out_detour = "";
+            List<string> detours = new List<string>();
+            List<string> declarations = new List<string>();
 
-            int pre_loc = signature.IndexOf('(');
-            string pre_part = ""; 
+            int counter = 0;
 
-            if (pre_loc >= 0)
-                pre_part = signature.Substring(0, pre_loc);
+            foreach (Signature sig in signatures)
+            {     
+                string desired_name = sig.name;
+                if (desired_name.Length == 0)
+                { 
+                    desired_name = "Unk" + counter.ToString();
+                    counter++;
+                }
 
-            int cdecl_loc = pre_part.IndexOf('_');
-            string cdecl_part = "";
+                string signature = sig.signature; //char __cdecl sub_1193D40(int a1, unsigned int a2, signed int *a3, _BYTE **a4, const char *a5)
+                string out_detour = "";
 
-            if (cdecl_loc >= 0)
-                cdecl_part = pre_part.Substring(cdecl_loc, 7);
+                int pre_loc = signature.IndexOf('(');
+                string pre_part = "";
 
-            int name_loc = pre_part.IndexOf("sub");
-            string name = "";
-
-            if (name_loc >= 0)
-                name = pre_part.Substring(name_loc, pre_loc - name_loc);
-
-            int address_loc = name.IndexOf('_');
-            string address = "";
-            if (address_loc >= 0)
-                address = name.Substring(address_loc + 1);
-
-            string arguments = signature.Substring(pre_loc + 1, signature.Length - (pre_loc + 1) - 1);
-            string[] arglist = arguments.Split(',');
-
-            string return_type = signature.Substring(0, cdecl_loc - 1);
-            out_detour = address;
-
-            out_detour = "#define " + desired_name.ToUpper() + "_ADDRESS 0x" + address + Environment.NewLine; // Address of function to hook
-            out_detour += return_type + "("; // Return type
-            out_detour += cdecl_part + "* "; // Declaration, cdecl supported right now
-            out_detour += "original" + desired_name + ")(";
-
-            string last = arglist.Last();
-
-            string argtypes = "";
-
-            foreach (string a in arglist)
-            {
-                string type = a.Substring(0, a.Length - 2);
-                type = type.Trim();
-                if (a == last)
-                    out_detour += type + ");";
+                if (pre_loc >= 0)
+                    pre_part = signature.Substring(0, pre_loc);
                 else
-                    out_detour += type + ", ";
+                    pre_part = signature;
 
-                if (a == last)
-                    argtypes += type;
+                int cdecl_loc = pre_part.IndexOf("__");
+                string cdecl_part = "";
+
+                if (cdecl_loc >= 0)
+                    cdecl_part = pre_part.Substring(cdecl_loc, 7);
+
+                int name_loc = pre_part.IndexOf("sub");
+                string name = "";
+
+                if (name_loc >= 0)
+                    name = pre_part.Substring(name_loc, (pre_loc >= 0 ? pre_loc : pre_part.Length) - name_loc);
+
+                int address_loc = name.IndexOf('_');
+                string address = "";
+                if (address_loc >= 0)
+                    address = name.Substring(address_loc + 1);
+
+                string arguments = "";
+                if (pre_loc >= 0)
+                    arguments = signature.Substring(pre_loc + 1, signature.Length - (pre_loc + 1) - 1);
+                
+                string[] arglist = arguments.Split(',');
+
+                string return_type = "";
+
+                if (cdecl_loc >= 0)
+                    return_type = signature.Substring(0, cdecl_loc - 1);
                 else
-                    argtypes += type + ", ";
+                    return_type = signature.Substring(0, signature.IndexOf("sub"));
+
+                out_detour = address;
+
+                out_detour = "#define " + desired_name.ToUpper() + "_ADDRESS 0x" + address + Environment.NewLine; // Address of function to hook
+                out_detour += return_type + "("; // Return type
+                out_detour += cdecl_part + "* "; // Declaration, cdecl supported right now
+                out_detour += "original" + desired_name + ")(";
+
+                string last = arglist.Last();
+
+                string argtypes = "";
+                if (arglist.Length == 1 && arglist[0].Length == 0)                
+                    out_detour += ");";                                    
+                else
+                {
+                    foreach (string a in arglist)
+                    {
+
+                        string type = a.Substring(0, a.Length - 2);
+                        type = type.Trim();
+                        if (a == last)
+                            out_detour += type + ");";
+                        else
+                            out_detour += type + ", ";
+
+                        if (a == last)
+                            argtypes += type;
+                        else
+                            argtypes += type + ", ";
+                    }
+                }
+
+
+
+                out_detour += Environment.NewLine;
+
+                out_detour += return_type + " " + "hk" + desired_name + "(";
+                if (arglist.Length == 1 && arglist[0].Length == 0)
+                    out_detour += ")";
+                else
+                {
+                    foreach (string a in arglist)
+                    {
+                        if (a == last)
+                            out_detour += a + ")";
+                        else
+                            out_detour += a + ", ";
+                    }
+                }
+
+                out_detour += Environment.NewLine + "{" + Environment.NewLine;
+
+                out_detour += "     return original" + desired_name + "(";
+
+                if (arglist.Length == 1 && arglist[0].Length == 0)
+                    out_detour += ");";
+                else
+                {
+                    foreach (string a in arglist)
+                    {
+                        string argname = a.Substring(a.Length - 2);
+
+                        if (a == last)
+                            out_detour += argname + ");";
+                        else
+                            out_detour += argname + ", ";
+                    }
+                }
+
+                out_detour += Environment.NewLine + "}";
+
+                out_detour += Environment.NewLine;
+                out_detour += Environment.NewLine;
+
+                if (cdecl_part.Length == 0)
+                    cdecl_part = "__stdcall";
+
+                string det = "original" + desired_name + " = (" + return_type + "(" + cdecl_part + "*)(" + argtypes + "))DetourFunction((PBYTE)" + desired_name.ToUpper() + "_ADDRESS, (PBYTE)hk" + desired_name + ");";
+
+                det = det.Replace("_BYTE", "BYTE");
+                out_detour = out_detour.Replace("_BYTE", "BYTE");                
+
+                detours.Add(det);
+                declarations.Add(out_detour);
             }
 
+            outputTextbox.Text = string.Join(Environment.NewLine, declarations);
+            outputTextbox.Text += string.Join(Environment.NewLine, detours);
 
-            out_detour += Environment.NewLine;
+        }
 
-            out_detour += return_type + " " + "hk" + desired_name + "(";
-            foreach (string a in arglist)
-            {
-                if (a == last)
-                    out_detour += a + ")";
-                else
-                    out_detour += a + ", ";
-            }
+        private void addSignatureBtn_Click(object sender, EventArgs e)
+        {
+            Signature sig = new Signature();
+            sig.name = functionNameTextBox.Text.Length > 0 ? functionNameTextBox.Text : "unkFunction";
+            sig.signature = functionSignatureTextBox.Text;
 
-            out_detour += Environment.NewLine + "{" + Environment.NewLine;
+            signatures.Add(sig);
 
-            out_detour += "return original" + desired_name + "(";
+            signatureListBox.Items.Add(functionNameTextBox.Text + " : " + functionSignatureTextBox.Text);
 
+            functionNameTextBox.Text = "";
+            functionSignatureTextBox.Text = "";
 
-            foreach (string a in arglist)
-            {
-                string argname = a.Substring(a.Length-2);
+        }
 
-                if (a == last)
-                    out_detour += argname + ");";
-                else
-                    out_detour += argname + ", ";
-            }
-
-            out_detour += Environment.NewLine + "}";
-
-            out_detour += Environment.NewLine;
-            out_detour += Environment.NewLine;
-
-            out_detour += "original" + desired_name + " = (" + return_type + "(" + cdecl_part + "*)(" + argtypes + "))DetourFunction((PBYTE)" + desired_name.ToUpper() + "_ADDRESS, (PBYTE)hk" + desired_name + ");";
-
-            out_detour = out_detour.Replace("_BYTE", "BYTE");
-
-            outputTextbox.Text = out_detour;
+        private void functionSignatureTextBox_Enter(object sender, EventArgs e)
+        {
+            if (functionSignatureTextBox.Text == "Example: char sub_12345()")
+                functionSignatureTextBox.Text = "";
         }
     }
 }
