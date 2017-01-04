f2c39224e11bb47f0575f4d0b8c48c2d038d3c6d
n00854180t <ross.pawley@gmail.com>
2017-01-03 16:20:33 -0800

    Implemented support for __cdecl style signatures.

---
+++ SignatureToDetour/signatureToDetourForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace SignatureToDetour
+{
+    partial class signatureToDetourForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.functionSignatureTextBox = new System.Windows.Forms.TextBox();
+            this.makeDetourBtn = new System.Windows.Forms.Button();
+            this.outputTextbox = new System.Windows.Forms.TextBox();
+            this.signatureLabel = new System.Windows.Forms.Label();
+            this.outputNameLabel = new System.Windows.Forms.Label();
+            this.functionNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // functionSignatureTextBox
+            // 
+            this.functionSignatureTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.functionSignatureTextBox.Location = new System.Drawing.Point(12, 114);
+            this.functionSignatureTextBox.Multiline = true;
+            this.functionSignatureTextBox.Name = "functionSignatureTextBox";
+            this.functionSignatureTextBox.Size = new System.Drawing.Size(426, 69);
+            this.functionSignatureTextBox.TabIndex = 0;
+            // 
+            // makeDetourBtn
+            // 
+            this.makeDetourBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.makeDetourBtn.Location = new System.Drawing.Point(12, 198);
+            this.makeDetourBtn.Name = "makeDetourBtn";
+            this.makeDetourBtn.Size = new System.Drawing.Size(420, 38);
+            this.makeDetourBtn.TabIndex = 2;
+            this.makeDetourBtn.Text = "Make Detour";
+            this.makeDetourBtn.UseVisualStyleBackColor = true;
+            this.makeDetourBtn.Click += new System.EventHandler(this.MakeDetour_Click);
+            // 
+            // outputTextbox
+            // 
+            this.outputTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputTextbox.Location = new System.Drawing.Point(12, 275);
+            this.outputTextbox.Multiline = true;
+            this.outputTextbox.Name = "outputTextbox";
+            this.outputTextbox.Size = new System.Drawing.Size(425, 286);
+            this.outputTextbox.TabIndex = 3;
+            // 
+            // signatureLabel
+            // 
+            this.signatureLabel.AutoSize = true;
+            this.signatureLabel.Location = new System.Drawing.Point(12, 95);
+            this.signatureLabel.Name = "signatureLabel";
+            this.signatureLabel.Size = new System.Drawing.Size(92, 13);
+            this.signatureLabel.TabIndex = 5;
+            this.signatureLabel.Text = "IDA Pro Signature";
+            // 
+            // outputNameLabel
+            // 
+            this.outputNameLabel.AutoSize = true;
+            this.outputNameLabel.Location = new System.Drawing.Point(12, 25);
+            this.outputNameLabel.Name = "outputNameLabel";
+            this.outputNameLabel.Size = new System.Drawing.Size(117, 13);
+            this.outputNameLabel.TabIndex = 6;
+            this.outputNameLabel.Text = "Output Function Name:";
+            // 
+            // functionNameTextBox
+            // 
+            this.functionNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.functionNameTextBox.Location = new System.Drawing.Point(12, 41);
+            this.functionNameTextBox.Name = "functionNameTextBox";
+            this.functionNameTextBox.Size = new System.Drawing.Size(422, 20);
+            this.functionNameTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Output:";
+            // 
+            // signatureToDetourForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 573);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputTextbox);
+            this.Controls.Add(this.makeDetourBtn);
+            this.Controls.Add(this.functionNameTextBox);
+            this.Controls.Add(this.outputNameLabel);
+            this.Controls.Add(this.signatureLabel);
+            this.Controls.Add(this.functionSignatureTextBox);
+            this.Name = "signatureToDetourForm";
+            this.Text = "Signature to Detour Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox functionSignatureTextBox;
+        private System.Windows.Forms.Button makeDetourBtn;
+        private System.Windows.Forms.TextBox outputTextbox;
+        private System.Windows.Forms.Label signatureLabel;
+        private System.Windows.Forms.Label outputNameLabel;
+        private System.Windows.Forms.TextBox functionNameTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SignatureToDetour/signatureToDetourForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignatureToDetour
+{
+    public partial class signatureToDetourForm : Form
+    {
+        public signatureToDetourForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MakeDetour_Click(object sender, EventArgs e)
+        {
+            string desired_name = functionNameTextBox.Text;
+            if (desired_name.Length == 0)
+                desired_name = "Unk001";
+
+            outputTextbox.Text = "";
+
+            string signature = functionSignatureTextBox.Text; //char __cdecl sub_1193D40(int a1, unsigned int a2, signed int *a3, _BYTE **a4, const char *a5)
+            string out_detour = "";
+
+            int pre_loc = signature.IndexOf('(');
+            string pre_part = ""; 
+
+            if (pre_loc >= 0)
+                pre_part = signature.Substring(0, pre_loc);
+
+            int cdecl_loc = pre_part.IndexOf('_');
+            string cdecl_part = "";
+
+            if (cdecl_loc >= 0)
+                cdecl_part = pre_part.Substring(cdecl_loc, 7);
+
+            int name_loc = pre_part.IndexOf("sub");
+            string name = "";
+
+            if (name_loc >= 0)
+                name = pre_part.Substring(name_loc, pre_loc - name_loc);
+
+            int address_loc = name.IndexOf('_');
+            string address = "";
+            if (address_loc >= 0)
+                address = name.Substring(address_loc + 1);
+
+            string arguments = signature.Substring(pre_loc + 1, signature.Length - (pre_loc + 1) - 1);
+            string[] arglist = arguments.Split(',');
+
+            string return_type = signature.Substring(0, cdecl_loc - 1);
+            out_detour = address;
+
+            out_detour = "#define " + desired_name.ToUpper() + "_ADDRESS 0x" + address + Environment.NewLine; // Address of function to hook
+            out_detour += return_type + "("; // Return type
+            out_detour += cdecl_part + "* "; // Declaration, cdecl supported right now
+            out_detour += "original" + desired_name + ")(";
+
+            string last = arglist.Last();
+
+            string argtypes = "";
+
+            foreach (string a in arglist)
+            {
+                string type = a.Substring(0, a.Length - 2);
+                type = type.Trim();
+                if (a == last)
+                    out_detour += type + ");";
+                else
+                    out_detour += type + ", ";
+
+                if (a == last)
+                    argtypes += type;
+                else
+                    argtypes += type + ", ";
+            }
+
+
+            out_detour += Environment.NewLine;
+
+            out_detour += return_type + " " + "hk" + desired_name + "(";
+            foreach (string a in arglist)
+            {
+                if (a == last)
+                    out_detour += a + ")";
+                else
+                    out_detour += a + ", ";
+            }
+
+            out_detour += Environment.NewLine + "{" + Environment.NewLine;
+
+            out_detour += "return original" + desired_name + "(";
+
+
+            foreach (string a in arglist)
+            {
+                string argname = a.Substring(a.Length-2);
+
+                if (a == last)
+                    out_detour += argname + ");";
+                else
+                    out_detour += argname + ", ";
+            }
+
+            out_detour += Environment.NewLine + "}";
+
+            out_detour += Environment.NewLine;
+            out_detour += Environment.NewLine;
+
+            out_detour += "original" + desired_name + " = (" + return_type + "(" + cdecl_part + "*)(" + argtypes + "))DetourFunction((PBYTE)UNK_ADDRESS, (PBYTE)hk" + desired_name + ");";
+
+            out_detour = out_detour.Replace("_BYTE", "BYTE");
+
+            outputTextbox.Text = out_detour;
+        }
+    }
+}
